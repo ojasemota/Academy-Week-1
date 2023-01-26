d45545a15f1687ba4cd6e6649102a30a0b62d725
Osariemen Asemota <ojasemota@gmail.com>
2023-01-26 14:10:11 -0700

    second commit

---
+++ src/Week1/Practise.java
@@ -0,0 +1,19 @@
+package Week1;
+
+public class Practise {
+
+	    public static void main(String[] args) {	    	
+	    	int arr[]= new int[]{10, 20, 30, 40, 50};
+	    	for(int i= 2; i < arr.length;i++);{
+	    	  int i = 0;
+	    		System.out.println(i);
+	    		System.out.println("Value="+arr);
+
+	    	
+}
+}
+}
+		
+	
+
+
--- src/Week1/Problem.java
+++ src/Week1/Problem.java
@@ -1,17 +1,18 @@
 package Week1;
 
+
+
 public class Problem {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-	
-		    
-		    
-	
-				
-			
-		
-
+		for(int i=2; i<=100; i=i+2) {
+	           System.out.println(i);
+	           }		
 	}
-
+	
 }
+	
+	
+
+
